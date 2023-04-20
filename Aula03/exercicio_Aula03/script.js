26a429bf25be6f12a63cc5e20dda82327f6dc806
erikadeolima <erikadeo.lima@hotmail.com>
2023-04-20 15:13:25 -0300

    Aula03: criando logica no script, e interagindo com o DOM

--- Aula03/exercicio_Aula03/script.js
+++ Aula03/exercicio_Aula03/script.js
@@ -4,10 +4,30 @@ for(let i = 0; i<=20;i=i+1){
   }
 }
 
-/* function vejaOsPares(){
-  let input = document.querySelector("#number");
-  let number = input.value;
-  for(let i = 0; i<=number;i=i+2){
-    console.log(`${i} é par!`)
+let button = document.getElementById("vejaOsPares");
+
+button.addEventListener("click", vejaOsPares);
+
+function vejaOsPares(){
+  let resultadoDiv = document.getElementById("resultadoDiv");
+  let pElements = document.getElementsByTagName("p");
+  // como inserir um elemento com algum parametro especifico como o id?
+  if(pElements.length !== 0){
+    Object.entries(pElements).forEach(element => {
+      const [postion, htmlElement]=element;
+      resultadoDiv.removeChild(htmlElement);
+    });
+  };
+
+  let input = document.getElementById("numberInput");
+  let number = input.value; 
+
+  for(let i = 0; i<=number;i=i+1){
+    if(i%2 ===0){
+      let createP = document.createElement("p");
+      let texto = document.createTextNode(`${i} é par!`);
+      createP.appendChild(texto);
+      resultadoDiv.appendChild(createP);
+    }
   }
-} */
+}
