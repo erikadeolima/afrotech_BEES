323e8ad099f9c06620dd99a8fad61c9639209f7b
erikadeolima <erikadeo.lima@hotmail.com>
2023-04-27 00:31:47 -0300

    feat: upgrading my portifolio

--- Projeto01-Portifólio/script.js
+++ Projeto01-Portifólio/script.js
@@ -2,7 +2,9 @@ let sendButton = document.getElementById("sendButton");
 let messageForm = document.getElementById("contact-form");
 let sucessfulMessage = document.getElementById("sucessful-message");
 let newMessageButton = document.getElementById("new-message");
-
+let contactLink = document.getElementById("link-contato");
+let contactDiv = document.getElementById("contact");
+let contactInfo = document.getElementById("contact-email");
 
 sendButton.addEventListener("click", sendMenssage);
 
@@ -16,4 +18,17 @@ newMessageButton.addEventListener("click", newMessage);
 function newMessage() {
   messageForm.style.display ="block";
   sucessfulMessage.style.display="none";
+}
+
+contactLink.addEventListener("click", scrollAllPage);
+
+function scrollAllPage() {
+  contactDiv.scrollIntoView();
+};
+
+contactInfo.addEventListener("click", copyMyContact)
+
+function copyMyContact(){
+  navigator.clipboard.writeText(contactInfo.innerText);
+  alert(`E-mail copiado: erikadeo.lima@hotmail.com`);
 }
