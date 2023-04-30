08c02168f05cfba003538773f5f2700d210bf4d1
erikadeolima <erikadeo.lima@hotmail.com>
2023-04-29 23:09:09 -0300

    feat: project02 updating

--- projeto02-review_page/src/Components/ReviewBook/ReviewBook.jsx
+++ projeto02-review_page/src/Components/ReviewBook/ReviewBook.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import readersInfo from '../../untils/readersInfo';
 import BookCard from '../BookCard/BookCard'
 import './ReviewBook.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 function ReviewBook(props){
+  const [currentIndex, setCurrentIndex] = useState();
+  function handleChange(index) {
+    setCurrentIndex(index);
+  };
   return(
     <div className="reviewBook">
     <h1 id="reviewBook-title">Últimas Avaliações</h1>
-    <div id="reviewed-books">
+    <Carousel
+      selectedItem={readersInfo[0].bookcase[currentIndex]}
+      onChange={handleChange}
+      className="carousel-container"
+      id="reviewed-books"
+    >
     {readersInfo[0].bookcase.map((book,i)=>(
       <BookCard
       key={book.titleBook}
@@ -17,7 +28,7 @@ function ReviewBook(props){
       reviewBook={book.reviewBook}
       />
     ))}
-    </div>
+    </Carousel>
   </div>
   );
 };
